634dc71e4859174135d418eecb8930c457879611
Robert Woerle <robert@linuxdevelopment.de>
2018-03-13 09:16:44 +0100

    compile.sh fixed
    Signed-off-by: Robert Woerle <robert@linuxdevelopment.de>

--- compile.sh
+++ compile.sh
@@ -2,4 +2,5 @@
 export ARCH=arm
 export CROSS_COMPILE=arm-poky-linux-gnueabi-
 
-make $1 CC="arm-poky-linux-gnueabi-gcc --sysroot=/data/mibtec-bsp-dev/fido-am437x/build/tmp/sysroots/am437x-evm/" $2
+#make $1 CC="arm-poky-linux-gnueabi-gcc --sysroot=/data/mibtec-bsp-dev/fido-am437x/build/tmp/sysroots/am437x-evm/" $2
+make $1 CC="arm-poky-linux-gnueabi-gcc --sysroot=/data/mibtec-bsp-dev/krogoth-am437x/build/tmp/sysroots/am437x-evm/" $2
